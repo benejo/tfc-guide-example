ba4e511d68965afa62ab446d19846f83145fd8c3
Bene Joseph <benejo@gmail.com>
2022-02-22 12:53:20 +0530

    Change to t2.small

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t3.small"
+  default     = "t2.small"
 }
 
 variable "instance_name" {
